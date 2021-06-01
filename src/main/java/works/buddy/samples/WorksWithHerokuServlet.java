c9cc2be1395340e5a2c0cc9ea7f9025d8a4171fe
Gyimesi Gábor <ggyimesi@inf.u-szeged.hu>
2021-06-01 20:18:15 +0200

    error

--- src/main/java/works/buddy/samples/WorksWithHerokuServlet.java
+++ src/main/java/works/buddy/samples/WorksWithHerokuServlet.java
@@ -7,11 +7,11 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-public class WorksWithHerokuServlet extends HttpServlet {
+public class WorksWithHerokuServlet extends HttpServlet 
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/plain");
-        response.setStatus(404);
+        response.setStatus((404);
         PrintWriter writer = response.getWriter();
         writer.print("Buddy Works with Heroku");
         writer.close();
